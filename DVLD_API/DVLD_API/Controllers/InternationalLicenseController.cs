7705a23e4080920649ea67c1ae55ce8a7596ea23
Rayan Alshehri <rayan10als@gmail.com>
2024-06-02 15:48:32 +0300

    Add international license controller

---
+++ DVLD_API/DVLD_API/Controllers/InternationalLicenseController.cs
@@ -0,0 +1,40 @@
+﻿using DVLD_Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DVLD_API.Controllers
+{
+    [Route("api/international-license")]
+    [ApiController]
+    public class InternationalLicenseController : ControllerBase
+    {
+        [HttpGet("{InternationalLicenseID:int}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<clsInternationalLicense> GetInternationalLicense(int InternationalLicenseID)
+        {
+            clsInternationalLicense License = clsInternationalLicense.Find(InternationalLicenseID);
+
+            if (License == null)
+            {
+                return NotFound("International license not found");
+            }
+
+            return Ok(License);
+        }
+
+        [HttpGet("is-expired/{InternationalLicenseID:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<bool> IsLicenseExpired(int InternationalLicenseID)
+        {
+            clsInternationalLicense License = clsInternationalLicense.Find(InternationalLicenseID);
+
+            if (License == null)
+            {
+                return BadRequest("International license not found");
+            }
+
+            return Ok(License.IsExpired());
+        }
+    }
+}
